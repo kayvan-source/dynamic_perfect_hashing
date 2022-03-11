037d90f4cc4298bb35cb078291cd4020aaa885d8
kayvan <kayvan.jamshidi@gmail.com>
2022-03-11 08:56:18 +0330

    add some associative function to the bucket impl

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,49 @@ impl Bucket {
             collisions : vec![],
         }
     }
+
+    pub fn key_ref(&self) -> &u64 {
+        &self.key
+    }
+    pub fn value_ref(&self) -> &u64 {
+        &self.value
+    }
+    pub fn index_ref(&self) -> &u64 {
+        &self.index
+    }
+    pub fn value_mut(&mut self) -> &mut u64 {
+        &mut self.value
+    }
+    pub fn index_mut(&mut self) -> &mut u64 {
+        &mut self.index
+    }
+    pub fn key(self) -> u64 {
+        self.key
+    }
+    pub fn value(self) -> u64 {
+        self.value
+    }
+    pub fn key_value(self) -> (u64, u64) {
+        (self.key, self.value)
+    }
+    pub fn refs(&self) -> (&u64, &u64) {
+        (&self.key, &self.value)
+    }
+    pub fn ref_mut(&mut self) -> (&u64, &mut u64) {
+        (&self.key, &mut self.value)
+    }
+    pub fn muts(&mut self) -> (&mut u64, &mut u64) {
+        (&mut self.key, &mut self.value)
+    }
+    pub fn contains_in_collisions(&self , item : &u64) -> bool {
+        self.collisions.contains(item)
+    }
+    pub fn add_to_collisions(&mut self, key : &u64) {
+        self.collisions.push(*key);
+    }
+    pub  fn update_index (&mut self , item : &u64){
+        self.index += item;
+    }
 }
 
 
