538f574ce649221662ce3d002faaab94f40f4be6
kayvan <kayvan.jamshidi@gmail.com>
2022-03-11 08:13:59 +0330

    add bucket struct to the project

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,15 @@ const KNUTH:u64 = 2654435769;
 const MOST_SIG: u64 = 16;
 type Collide = (u64, u64);
 
+#[derive(Clone,Debug)]
+pub struct Bucket
+{
+    key: u64,
+    value: u64,
+    index: u64,
+    collisions: Vec<u64>,
+}
+
 
 
 
