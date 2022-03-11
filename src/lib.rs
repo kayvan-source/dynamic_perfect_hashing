ba5d0ae2471d06533b9b0e385ea68495bde513b5
kayvan <kayvan.jamshidi@gmail.com>
2022-03-11 08:11:53 +0330

    add some constant to the project

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+
+
+const CAPACITY: u64 = 16;
+const FACTOR: f64 = 0.75;
+const KNUTH:u64 = 2654435769;
+const MOST_SIG: u64 = 16;
+type Collide = (u64, u64);
+
+
+
+
+
+
+
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
